7f5feb5acea74abe1662b22741a679b53ee9a36c
Catargo <cobauper@gmail.com>
2024-06-19 16:09:34 +0200

    Removed old plugin activation

--- src/start.server.ts
+++ src/start.server.ts
@@ -1,11 +1,3 @@
 import { PlguinGetter } from "plugin_getter";
 
 PlguinGetter.SetPlugin(plugin);
-
-//small hardcoded way of reinabling the plugin, if focus lost plugin deactivates itself
-//if plugin is not active mouse will not work;
-plugin.Activate(true);
-//reactivates the plugin
-plugin.Deactivation.Connect(() => {
-  plugin.Activate(true);
-})
